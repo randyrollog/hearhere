574c5a5bdaae5bb28eb34cb8f11e39a29805f4a4
cspears2002 <cspears2002@yahoo.com>
2013-11-07 16:37:00 -0800

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HearHere::Application.config.secret_key_base = '26f1886d5b92f227dc802432f2c8001b61ceba3688d65d2bb2fbc56b00c496faec6d91ac9b239d554841ec9d278a5598887a95067b46e607ad7dcc2a4314fc6c'
