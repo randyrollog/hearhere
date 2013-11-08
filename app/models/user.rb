23c6484782965fa79f46396de105ec706b71b5cb
cspears2002 <cspears2002@yahoo.com>
2013-11-07 18:28:03 -0800

    Got bcrypt up and running.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,21 @@ class User < ActiveRecord::Base
 	has_many :sounds
   # has_many :tags
 
+  before_save :hash_password
+
   def authenticate(password)
     self.hashed_password ==
     BCrypt::Engine.hash_secret(password, self.salt)
+  end
+
+  private
+
+  def hash_password
+    if password.present?
+      self.salt = BCrypt::Engine.generate_salt
+      self.hashed_password = BCrypt::Engine.hash_secret(password, self.salt)
+      password = password_confirmation = nil
+    end
+  end
+
 end
