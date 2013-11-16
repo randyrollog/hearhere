39b68d5214971d89d5e66414affc2ef252135bce
cspears2002 <cspears2002@yahoo.com>
2013-11-16 15:07:00 -0800

    Got file downloads from AWS working with Paperclip.

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -44,16 +44,6 @@ class SoundsController < ApplicationController
       :x_sendfile => true
   end
 
-  # def download_url(sound)
-  #   s3 = AWS::S3.new
-  #   bucket = s3.buckets['hearherebucket']
-  #   object = bucket.objects['/sounds/sound_files/000/000/005/original/83746__braffe2__pen-writing.wav']
-  #   object.url_for(:get, { 
-  #     expires: 10.minutes,
-  #     response_content_disposition: 'attachment;'
-  #   }).to_s
-  # end
-
   private
 
   # Use strong_parameters for attribute whitelisting
