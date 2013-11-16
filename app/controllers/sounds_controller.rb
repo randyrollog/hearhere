51cbe6f1c68865c3fe38ca431d6bfaa05ca80895
cspears2002 <cspears2002@yahoo.com>
2013-11-16 13:51:36 -0800

    Can now download a text file with a path to the sound file.  Tweaking the controller to see if I can get an actual sound file.

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -37,8 +37,9 @@ class SoundsController < ApplicationController
 
   def download
     @sound= Sound.find(params[:id])
-    send_data @sound.sound_file, :disposition => 'attachment', :x_sendfile => true
-    # redirect_to download_url(@sound)
+    send_data @sound.sound_file, :type => @sound.sound_file_content_type,
+      :disposition => 'attachment', 
+      :x_sendfile => true
   end
 
   # def download_url(sound)
