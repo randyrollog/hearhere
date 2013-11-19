ee1f5bfd650cb2d07dcd6bb1812aaacfb0aded34
cspears2002 <cspears2002@yahoo.com>
2013-11-19 09:10:57 -0800

    Removed tags model and dropped tags table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131113014229) do
+ActiveRecord::Schema.define(version: 20131119170407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,12 +38,6 @@ ActiveRecord::Schema.define(version: 20131113014229) do
   end
 
   add_index "sounds", ["user_id"], name: "index_sounds_on_user_id", using: :btree
-
-  create_table "tags", force: true do |t|
-    t.string   "tag_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "users", force: true do |t|
     t.string   "user_name"
