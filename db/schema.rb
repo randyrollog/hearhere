0675de1c3eaadffb7b33ad2e0038f53aaedcc7ee
cspears2002 <cspears2002@yahoo.com>
2013-11-19 16:46:33 -0800

    Ran db:migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131119171007) do
+ActiveRecord::Schema.define(version: 20131120002413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20131119171007) do
   create_table "robots", force: true do |t|
     t.string   "robot_name"
     t.string   "model"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sound_ratings", force: true do |t|
+    t.integer  "value"
+    t.integer  "sound_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
